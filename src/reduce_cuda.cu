8aea20f0d6a546e8e3372af97faed69a3e302969
alexgo <alex.goscinski@posteo.de>
2022-09-22 18:39:16 +0200

    added implementation removing the for-loop in the cuda kernel replacing it with 2d block and threads

--- src/reduce_cuda.cu
+++ src/reduce_cuda.cu
@@ -10,12 +10,21 @@ __global__ void reduce_backward_cuda_kernel(
     int32_t n_samples,
     int32_t other_sizes
 ) {
+    // SOLUTION with for-loop in cuda kernel
+    //int i = blockIdx.x * blockDim.x + threadIdx.x;
+    //if (i < n_samples) {
+    //    auto reduce_id = mapping[i];
+    //    for (int j=0; j<other_sizes; j++) {
+    //        full[i * other_sizes + j] = reduced[reduce_id * other_sizes + j];
+    //    }
+    //}
+
+    // SOLUTION without for-loop in cuda kernel
     int i = blockIdx.x * blockDim.x + threadIdx.x;
-    if (i < n_samples) {
-        auto reduce_id = mapping[i];
-        for (int j=0; j<other_sizes; j++) {
-            full[i * other_sizes + j] = reduced[reduce_id * other_sizes + j];
-        }
+    int j = blockIdx.y * blockDim.y + threadIdx.y;
+    if (i < n_samples && j < other_sizes) {
+       auto reduce_id = mapping[i];
+       full[i * other_sizes + j] = reduced[reduce_id * other_sizes + j];
     }
 }
 
@@ -29,11 +38,17 @@ void reduce_backward_cuda(
     CHECK_CUDA(full);
     CHECK_CUDA(reduced);
     CHECK_CUDA(mapping);
+    
+    // SOLUTION with for-loop in cuda kernel
+    //int threads = 64;
+    //int blocks = (n_samples + threads - 1) / threads;
 
-    int threads = 64;
-    int blocks = (n_samples + threads - 1) / threads;
+    // SOLUTION without for-loop in cuda kernel 
+    //const dim3 blocks((n_samples + threads - 1) / threads, (other_sizes + threads - 1) / threads); // tried with one thread but this did not work
+    const dim3 threads(8,8);
+    const dim3 blocks((n_samples + threads.x - 1) / threads.x, (other_sizes + threads.y - 1) / threads.y);
 
-    AT_DISPATCH_FLOATING_TYPES(full.type(), "reduce_backward_cpu", ([&] {
+    AT_DISPATCH_FLOATING_TYPES(full.type(), "reduce_backward_cuda", ([&] {
         reduce_backward_cuda_kernel<<<blocks, threads>>>(
             full.data_ptr<scalar_t>(),
             reduced.data_ptr<scalar_t>(),
